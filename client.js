15d6c843c92d77e604595ae209c591ddd68a87dd
Christopher Black <chris@blacktopinteractive.com>
2022-06-07 21:31:51 -0500

    Code from lecture.

---
+++ client.js
@@ -0,0 +1,17 @@
+// JavaScript code comment
+/**
+ * Multi-line code comment
+ */
+
+// This will output to the browser
+// console.
+console.log('Hello world!');
+
+// Create a variable that stores
+// the current year.
+let currentYear = 2022; // number
+let pizzaTopping = 'mushrooms'; // string
+let isLeapYear = false; // boolean
+
+// The current year is 2022
+console.log('The current year is', currentYear);
